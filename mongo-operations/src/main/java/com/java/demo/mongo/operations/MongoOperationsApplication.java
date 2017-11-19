b754646ec005a318399a6934f9adf4332344ecc3
php9527 <229333544@qq.com>
2017-11-19 16:37:29 +0800

    init project

---
+++ mongo-operations/src/main/java/com/java/demo/mongo/operations/MongoOperationsApplication.java
@@ -0,0 +1,12 @@
+package com.java.demo.mongo.operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoOperationsApplication.class, args);
+	}
+}
---
+++ mongo-operations/src/test/java/com/java/demo/mongo/operations/MongoOperationsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.java.demo.mongo.operations;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongoOperationsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
